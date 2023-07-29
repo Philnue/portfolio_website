8650a4cfbf1fdc50b00af5936bd96e0b634025f3
Philipp <philnue@gmail.com>
2023-07-29 09:17:04 +0200

    Added all files

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,75 @@
+import { Link } from "react-scroll";
+import {
+    FaGithub,
+    FaLinkedin,
+} from 'react-icons/fa';
+import { HiOutlineMail } from "react-icons/hi";
+
+const Navbar = () => {
+    return (
+        <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+            <div className="w-[200px]">IMAGE</div>
+
+            {/* menu */}
+            <ul className="hidden md:flex">
+                <li>
+                    <Link to="home" smooth={true} duration={500}>
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <Link to="about" smooth={true} duration={500}>
+                        About
+                    </Link>
+                </li>
+                <li>
+                    <Link to="skills" smooth={true} duration={500}>
+                        Skills
+                    </Link>
+                </li>
+                <li>
+                    <Link to="work" smooth={true} duration={500}>
+                        Work
+                    </Link>
+                </li>
+                <li>
+                    <Link to="contact" smooth={true} duration={500}>
+                        Contact
+                    </Link>
+                </li>
+            </ul>
+
+            {/* Social icons */}
+            <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
+                <ul>
+                    <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
+                        <a
+                            className="flex justify-between items-center w-full text-gray-300"
+                            href="/"
+                        >
+                            Linkedin <FaLinkedin size={30} />
+                        </a>
+                    </li>
+                    <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
+                        <a
+                            className="flex justify-between items-center w-full text-gray-300"
+                            href="/"
+                        >
+                            Github <FaGithub size={30} />
+                        </a>
+                    </li>
+                    <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]">
+                        <a
+                            className="flex justify-between items-center w-full text-gray-300"
+                            href="/"
+                        >
+                            Email <HiOutlineMail size={30} />
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ src/data/data.ts
@@ -0,0 +1,32 @@
+import React from "../assets/react.svg";
+
+export const data = [
+  {
+    id: 1,
+    name: "React JS Application",
+    image: React,
+    github: "",
+    live: "",
+  },
+  // {
+  //   id: 2,
+  //   name: "React JS Application",
+  //   image: RealEst,
+  //   github: "",
+  //   live: "",
+  // },
+  // {
+  //   id: 3,
+  //   name: "React JS Application",
+  //   image: WorkIm,
+  //   github: "",
+  //   live: "",
+  // },
+  // {
+  //   id: 4,
+  //   name: "React JS Application",
+  //   image: RealEst,
+  //   github: "",
+  //   live: "",
+  // },
+];
