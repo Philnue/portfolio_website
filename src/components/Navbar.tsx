dca1aa1a96679cd8e66a4308e17aaed6c5375927
Philipp <philnue@gmail.com>
2023-07-29 09:52:58 +0200

    Update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -45,7 +45,7 @@ const Navbar = () => {
                     <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
                         <a
                             className="flex justify-between items-center w-full text-gray-300"
-                            href="/"
+                            href="https://de.linkedin.com/in/philipp-n%C3%BC%C3%9Flein-8b63b8218"
                         >
                             Linkedin <FaLinkedin size={30} />
                         </a>
@@ -53,7 +53,7 @@ const Navbar = () => {
                     <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
                         <a
                             className="flex justify-between items-center w-full text-gray-300"
-                            href="/"
+                            href="https://www.github.com/Philnue"
                         >
                             Github <FaGithub size={30} />
                         </a>
